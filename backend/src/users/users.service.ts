1d4bb727f890c94801609a9e6c2540f6205b7596
abbas-fakih39 <abbassfakih54321@outlook.com>
2026-06-03 05:41:00 +0200

    feat: add UsersModule with UsersService

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { Prisma } from '../../generated/prisma';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  findByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+
+  findById(id: string) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+
+  create(data: Prisma.UserCreateInput) {
+    return this.prisma.user.create({ data });
+  }
+}
